510f694a67d5506454a8e30956b62592e3a7e5d0
Armen Mkryanh <armenmkryan@gmail.com>
2022-12-18 16:13:35 +0400

    lesson 1

---
+++ myfirstreact/src/Components/Header/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+class Header extends React.Component{
+    render(){
+        return <div>
+    <h1>Hello, WORLD!!!</h1>
+    </div> }
+}
+export default Header;
